c45175207cf76b3f4051cb14d00efff91efa5ecc
Olga <49266934+olgakos@users.noreply.github.com>
2022-05-20 14:22:15 +0300

    Add files via upload

---
+++ Specification/spec/ApiRequestSpecification.java
@@ -0,0 +1,23 @@
+package Specification.spec;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static filter.CustomLogFilter.customLogFilter;
+import static io.restassured.RestAssured.with;
+
+public class ApiRequestSpecification {
+
+    public static RequestSpecification requestReqresSpec = with()
+            .baseUri("https://reqres.in")
+            .filter(customLogFilter().withCustomTemplates())
+            .log().all()
+            .contentType(ContentType.JSON);
+
+
+    public static ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .build();
+}
